b6c80dd75efad2663a625fcf99808a8bac59d8d7
Tahreem-riaz <totehreem101@gmail.com>
2026-09-05 23:59:18 +0500

    Add question of program no 9

--- 01_fundamentals.py
+++ 01_fundamentals.py
@@ -130,3 +130,8 @@ volume = length * width * height
 print(volume)
 
 # --------------------------------------------------------
+
+# Q9. Swap two variables WITHOUT using a third variable.
+#     Start with: x = 5, y = 10
+#     After swap: x should be 10, y should be 5
+#     Print both after swapping.
