11b2a5413793f8f3415cda2f5403c56fe19f4ff6
Tahreem-riaz <totehreem101@gmail.com>
2026-09-06 23:24:12 +0500

    Upgrade the topic names and done some changes in topics

--- 03_operators.py
+++ 03_operators.py
@@ -2,8 +2,7 @@
 ========================================================
            LECTURE 1 - FILE 3: OPERATORS
 ========================================================
-Topics: Arithmetic, Comparison, Assignment, Logical,
-        Membership, Identity, Bitwise Operators
+Topics: Arithmetic, Comparison, Assignment, Logical Operators
 ========================================================
 """
 
