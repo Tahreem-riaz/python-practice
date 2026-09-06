444c8bf39974c367f0a67d71ad4857e54664eaaa
Tahreem-riaz <totehreem101@gmail.com>
2026-09-06 21:21:41 +0500

    Fix the problem and add the Topics: Arithmetic, Comparison, Assignment, Logical,         Membership, Identity, Bitwise Operators

---
+++ 03_operators.py
@@ -0,0 +1,8 @@
+"""
+========================================================
+           LECTURE 1 - FILE 3: OPERATORS
+========================================================
+Topics: Arithmetic, Comparison, Assignment, Logical,
+        Membership, Identity, Bitwise Operators
+========================================================
+"""
