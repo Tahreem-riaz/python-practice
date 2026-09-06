dd93c406fa34441966f9fd3eccf69150d3fc22d7
Tahreem-riaz <totehreem101@gmail.com>
2026-09-06 06:05:20 +0500

    Add the question of peogram no 1

--- 02_data_types.py
+++ 02_data_types.py
@@ -10,3 +10,9 @@ Total Questions: 12
 # ========================================================
 # PART A: INTEGER AND FLOAT
 # ========================================================
+
+# Q1. A library has 245 books and 18.5 kg of donated
+#     materials.
+#
+#     Create variables for both values.
+#     Print each value and its data type.
